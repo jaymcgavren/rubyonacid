8f060b6eba1524bc1aa3a871768a078631eb66b9
Jay McGavren <jay@mcgavren.com>
2009-10-13 15:13:48 -0700

    MetaFactory#factories() renamed to #factory_pool().

--- lib/rubyonacid/factories/meta.rb
+++ lib/rubyonacid/factories/meta.rb
@@ -4,15 +4,20 @@ module RubyOnAcid
 
 class MetaFactory < Factory
   
-  attr_accessor :factories
+  attr_accessor :factory_pool
+  attr_accessor :assigned_factories
   
-  def initialize
-    @factories = []
+  def initialize(factory_pool = [])
+    @factory_pool = factory_pool
     @assigned_factories = {}
   end
   
+  def assign_factory(key, factory)
+    @assigned_factories[key] = factory
+  end
+  
   def get_unit(key)
-    @assigned_factories[key] ||= @factories[rand(@factories.length)]
+    @assigned_factories[key] ||= @factory_pool[rand(@factory_pool.length)]
     @assigned_factories[key].get_unit(key)
   end
   
