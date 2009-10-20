2c8e76c46a60649f44742891d878274542cf8d6d
Jay McGavren <jay@mcgavren.com>
2009-10-19 23:56:40 -0700

    Added timeout to RindaGenerator.

--- lib/rubyonacid/factories/rinda.rb
+++ lib/rubyonacid/factories/rinda.rb
@@ -5,11 +5,15 @@ module RubyOnAcid
 
 class RindaFactory < Factory
   
+  #Time in seconds to wait for a value before giving up and returning the last retrieved value for the given key.
+  attr_accessor :timeout
+  #The URI to connect to.  Default is "druby://127.0.0.1:7632" (7632 == RNDA).
   attr_accessor :uri
   
-  #Takes the URI to connect to.  Default is "druby://127.0.0.1:7632" (7632 == RNDA).
-  def initialize(uri = "druby://127.0.0.1:7632")
+  def initialize(uri = "druby://127.0.0.1:7632", timeout = 1)
     @uri = uri
+    @timeout = timeout
+    @prior_values = {}
   end
   
   def start_service
@@ -19,7 +23,13 @@ class RindaFactory < Factory
   
   #Get key from Rinda server.
   def get_unit(key)
-    key, value = @space.take([key, Float])
+    @prior_values[key] ||= 0.0
+    begin
+      key, value = @space.take([key, Float], @timeout)
+      @prior_values[key] = value
+    rescue Rinda::RequestExpiredError => exception
+      value = @prior_values[key]
+    end
     value
   end
 
