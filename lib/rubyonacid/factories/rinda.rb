0d220e80d45340c3f8afee4d0c264806ad4847d5
Jay McGavren <jay@mcgavren.com>
2009-12-01 00:27:15 -0700

    Added RindaFactory#default_factory.

--- lib/rubyonacid/factories/rinda.rb
+++ lib/rubyonacid/factories/rinda.rb
@@ -5,9 +5,11 @@ module RubyOnAcid
 
 class RindaFactory < Factory
   
-  #Time in seconds to wait for a value before giving up and returning the last retrieved value for the given key.
+  #Time in seconds to wait for a value before giving up and returning a default value for the given key.
   #Default is 0, which will return immediately.
   attr_accessor :timeout
+  #A factory to pull requests from if retrieval of values via Rinda times out.
+  attr_accessor :default_factory
   #The URI to connect to.  Default is "druby://127.0.0.1:7632" (7632 == RNDA).
   attr_accessor :uri
   
@@ -15,6 +17,7 @@ class RindaFactory < Factory
     super
     @uri = uri
     @timeout = timeout
+    @default_factory = nil
     @prior_values = {}
   end
   
@@ -30,7 +33,11 @@ class RindaFactory < Factory
       key, value = @space.take([key, Float], @timeout)
       @prior_values[key] = value
     rescue Rinda::RequestExpiredError => exception
-      value = @prior_values[key]
+      if @default_factory
+        value = @default_factory.get_unit(key)
+      else
+        value = @prior_values[key]
+      end
     end
     value
   end
