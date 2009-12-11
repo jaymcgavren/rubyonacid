bc35d2d4db4fd8e28a07c01f293925ca1e10ec5e
Jay McGavren <jay@mcgavren.com>
2009-12-10 21:13:48 -0700

    Added InputFactory, which lets you queue specific values to be pulled out later.

---
+++ lib/rubyonacid/factories/input.rb
@@ -0,0 +1,63 @@
+require 'rubyonacid/factory'
+
+module RubyOnAcid
+
+class InputFactory < Factory
+  
+  def initialize
+    super
+    @input_values = {}
+    @key_assignments = {}
+    @largest_seen_values = {}
+    @smallest_seen_values = {}
+  end
+  
+  def get_unit(key)
+    current_key = assigned_key(key)
+    if @input_values[current_key] and @input_values[current_key].length > 0
+      return scale(current_key, @input_values[current_key].shift) || default_value(key)
+    else
+      return default_value(key)
+    end
+  end
+  
+  def default_value(key)
+    # 0.0
+    0.75
+  end
+  
+  def put(key, value)
+    value = value.to_f
+    @input_values[key] ||= []
+    @input_values[key] << value
+    @smallest_seen_values[key] ||= 0.0
+    if @largest_seen_values[key] == nil or @smallest_seen_values[key] > @largest_seen_values[key]
+      @largest_seen_values[key] = @smallest_seen_values[key] + 1.0
+    end
+    @smallest_seen_values[key] = value if value < @smallest_seen_values[key]
+    @largest_seen_values[key] = value if value > @largest_seen_values[key]
+  end
+  
+  def clear_input_values
+    @input_values = {}
+  end
+  
+  private
+  
+    def assigned_key(key)
+      return @key_assignments[key] if @key_assignments[key]
+      key_pool = @input_values.keys - @key_assignments.values
+      @key_assignments[key] = key_pool[rand(key_pool.length)]
+      @key_assignments[key]
+    end
+    
+    #Scales a value between the largest and smallest values seen for a key.
+    #Returns a value in the range 0 to 1.
+    def scale(key, value)
+# require 'g'; g "#{@largest_seen_values}/#{@smallest_seen_values}"
+      (value - @smallest_seen_values[key]) / (@largest_seen_values[key] - @smallest_seen_values[key])
+    end
+  
+end
+
+end
---
+++ spec/factories/input_spec.rb
@@ -0,0 +1,96 @@
+require File.join(File.dirname(__FILE__), '..', 'spec_helper')
+require "rubyonacid/factories/input"
+require "shared_factory_specs"
+
+include RubyOnAcid
+
+describe InputFactory do
+
+  before :each do
+    @it = InputFactory.new
+  end
+
+  describe "general behavior" do
+
+    before :each do
+      @it.put(:foo, 0.0)
+      @it.put(:foo, 1.0)
+    end
+
+    it_should_behave_like "a factory"
+    
+  end
+  
+  describe "#get_unit" do
+    
+    it "retrieves values assigned to a key" do
+      @it.put(:x, 0.1)
+      @it.get(:x).should == 0.1
+    end
+    
+    it "retrieves multiple input values in order" do
+      @it.put(:x, 0.1)
+      @it.put(:x, 0.2)
+      @it.get(:x).should == 0.1
+      @it.get(:x).should == 0.2
+    end
+    
+    it "maps to a different key if the requested one isn't present" do
+      @it.put(:x, 0.1)
+      @it.get(:y).should == 0.1
+    end
+    
+    it "returns 0 if a key is assigned but has no values" do
+      @it.put(:x, 0.1)
+      @it.put(:x, 0.2)
+      @it.get(:y).should == 0.1
+      @it.get(:y).should == 0.2
+      @it.put(:z, 0.3)
+      @it.get(:y).should == 0.0
+    end
+  
+  end
+  
+  describe "scaling" do
+    
+    it "scales highest seen values for a key to 0 to 1 range" do
+      @it.put(:x, 0.0)
+      @it.put(:x, 1.0)
+      @it.get(:x).should be_close(0.0, MARGIN)
+      @it.get(:x).should be_close(1.0, MARGIN)
+      @it.put(:x, 0.0)
+      @it.put(:x, 1.0)
+      @it.put(:x, 2.0)
+      @it.get(:x).should be_close(0.0, MARGIN)
+      @it.get(:x).should be_close(0.5, MARGIN)
+      @it.get(:x).should be_close(1.0, MARGIN)
+      @it.put(:x, 0.0)
+      @it.put(:x, 3.0)
+      @it.put(:x, 4.0)
+      @it.get(:x).should be_close(0.0, MARGIN)
+      @it.get(:x).should be_close(0.75, MARGIN)
+      @it.get(:x).should be_close(1.0, MARGIN)
+    end
+    
+    it "scales lowest seen values for a key to 0 to 1 range" do
+      @it.put(:x, 0.0)
+      @it.put(:x, 1.0)
+      @it.get(:x).should be_close(0.0, MARGIN)
+      @it.get(:x).should be_close(1.0, MARGIN)
+      @it.put(:x, 0.0)
+      @it.put(:x, 1.0)
+      @it.put(:x, -2.0)
+      @it.get(:x).should be_close(0.666, MARGIN)
+      @it.get(:x).should be_close(1.0, MARGIN)
+      @it.get(:x).should be_close(0.0, MARGIN)
+      @it.put(:x, -2.0)
+      @it.put(:x, 2.0)
+      @it.put(:x, 1.0)
+      @it.get(:x).should be_close(0.0, MARGIN)
+      @it.get(:x).should be_close(1.0, MARGIN)
+      @it.get(:x).should be_close(0.75, MARGIN)
+    end
+    
+  end
+  
+end
