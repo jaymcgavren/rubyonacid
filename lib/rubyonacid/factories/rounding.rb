681a355afa8b4959975e6aba28b91eeedfe1b3ef
Jay McGavren <jay@mcgavren.com>
2010-05-15 01:59:42 -0700

    RoundingFactory now uses source_factories.

--- lib/rubyonacid/factories/rounding.rb
+++ lib/rubyonacid/factories/rounding.rb
@@ -5,19 +5,16 @@ module RubyOnAcid
 #Rounds values from a source factory, useful for clustering values into groups.
 class RoundingFactory < Factory
   
-  #Factory to get values from.
-  attr_accessor :source_factory
   #Source values will be rounded to the nearest multiple of this value.
   attr_accessor :nearest
   
   def initialize(options = {})
     super
-    @source_factory = options[:source_factory]
     @nearest = options[:nearest] || 0.1
   end
   
   def get_unit(key)
-    round_to(@source_factory.get_unit(key), @nearest)
+    round_to(super, @nearest)
   end
 
   private
--- spec/factories/rounding_spec.rb
+++ spec/factories/rounding_spec.rb
@@ -13,7 +13,7 @@ describe RoundingFactory do
   describe "general behavior" do
   
     before :each do
-      @it.source_factory = mock('Factory', :get_unit => 0.2)
+      @it.source_factories << mock('Factory', :get_unit => 0.2)
     end
   
     it_should_behave_like "a factory"
@@ -22,7 +22,7 @@ describe RoundingFactory do
   
   it "Requests a value from the source factory and rounds it to a multiple of the requested number" do
     source_factory = mock('Factory')
-    @it.source_factory = source_factory
+    @it.source_factories << source_factory
     @it.nearest = 0.3
     source_factory.should_receive(:get_unit).and_return(0.7)
     @it.get_unit(:x).should be_close(0.6, MARGIN)
@@ -35,32 +35,34 @@ describe RoundingFactory do
   end
   
   it "can round to multiples of 0.2" do
-    @it.source_factory = mock('Factory')
+    source_factory = mock('Factory')
+    @it.source_factories << source_factory
     @it.nearest = 0.2
-    @it.source_factory.should_receive(:get_unit).and_return(0.0)
+    source_factory.should_receive(:get_unit).and_return(0.0)
     @it.get_unit(:x).should be_close(0.0, MARGIN)
-    @it.source_factory.should_receive(:get_unit).and_return(0.11)
+    source_factory.should_receive(:get_unit).and_return(0.11)
     @it.get_unit(:x).should be_close(0.2, MARGIN)
-    @it.source_factory.should_receive(:get_unit).and_return(0.2)
+    source_factory.should_receive(:get_unit).and_return(0.2)
     @it.get_unit(:x).should be_close(0.2, MARGIN)
-    @it.source_factory.should_receive(:get_unit).and_return(0.31)
+    source_factory.should_receive(:get_unit).and_return(0.31)
     @it.get_unit(:x).should be_close(0.4, MARGIN)
-    @it.source_factory.should_receive(:get_unit).and_return(0.4)
+    source_factory.should_receive(:get_unit).and_return(0.4)
     @it.get_unit(:x).should be_close(0.4, MARGIN)
   end
   
   it "can round to multiples of 1.0" do
-    @it.source_factory = mock('Factory')
+    source_factory = mock('Factory')
+    @it.source_factories << source_factory
     @it.nearest = 1.0
-    @it.source_factory.should_receive(:get_unit).and_return(0.0)
+    source_factory.should_receive(:get_unit).and_return(0.0)
     @it.get_unit(:x).should be_close(0.0, MARGIN)
-    @it.source_factory.should_receive(:get_unit).and_return(0.4)
+    source_factory.should_receive(:get_unit).and_return(0.4)
     @it.get_unit(:x).should be_close(0.0, MARGIN)
-    @it.source_factory.should_receive(:get_unit).and_return(0.51)
+    source_factory.should_receive(:get_unit).and_return(0.51)
     @it.get_unit(:x).should be_close(1.0, MARGIN)
-    @it.source_factory.should_receive(:get_unit).and_return(0.9)
+    source_factory.should_receive(:get_unit).and_return(0.9)
     @it.get_unit(:x).should be_close(1.0, MARGIN)
-    @it.source_factory.should_receive(:get_unit).and_return(1.0)
+    source_factory.should_receive(:get_unit).and_return(1.0)
     @it.get_unit(:x).should be_close(1.0, MARGIN)
   end
   
